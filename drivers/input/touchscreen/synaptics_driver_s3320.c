f5cc9e3c415a17ccecf780a14903bdf9a87ade46
engstk <eng.stk@sapo.pt>
2016-12-08 03:57:42 +0000

    input: synaptics: s3320: fix gestures
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1217,6 +1217,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
             //#endif, ruanbanmao@bsp 2015-05-06, end.
 	}
 
+/*
 // carlo@oneplus.net 2015-05-25, begin.
 	keyCode = UnkownGestrue;
 	// Get key code based on registered gesture.
@@ -1255,6 +1256,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 			break;
 	}
 // carlo@oneplus.net 2015-05-25, end.
+*/
 
 	TPD_ERR("detect %s gesture\n", gesture == DouTap ? "(double tap)" :
 			gesture == UpVee ? "(V)" :
