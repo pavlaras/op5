1aa8c57fb02058e87036a91a3789cc3ee7336e2f
Francisco Franco <franciscofranco.1990@gmail.com>
2017-08-20 18:49:10 +0100

    wakeup: block more possible wlan wakelocks
    I've seen these pop every now and then and they are a mistery to
    me since they're part of the wlan userspace module. The PNO is somehow
    related to network scans, the other one I have no clue.
    
    I'll disable them by default for now to see how it goes.
    
    Notice to all you cherry pickers: don't pick this unless you know
    your device is acquiring them. I haven't seen these before so don't
    merge this for no reason. Also if you merge it, don't @ my name,
    I'm tired of receiving stupid notifications. If you want to merge this
    just retain my sign-off and/or add me as the author. That way I won't
    get spammed to high heavens by 239482394 different cherry pickers.
    
    Make sure these wakelocks are always relevant for your devices, don't
    merge for the sake of merging.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -36,6 +36,10 @@ static bool enable_netlink_ws = true;
 module_param(enable_netlink_ws, bool, 0644);
 static bool enable_netmgr_wl_ws = true;
 module_param(enable_netmgr_wl_ws, bool, 0644);
+static bool enable_wlan_ipa_ws = true;
+module_param(enable_wlan_ipa_ws, bool, 0644);
+static bool enable_wlan_pno_wl_ws = true;
+module_param(enable_wlan_pno_wl_ws, bool, 0644);
 static bool enable_timerfd_ws = true;
 module_param(enable_timerfd_ws, bool, 0644);
 
@@ -595,6 +599,10 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 				!strncmp(ws->name, "NETLINK", wslen)) ||
 			(!enable_netmgr_wl_ws &&
 				!strncmp(ws->name, "netmgr_wl", wslen)) ||
+			(!enable_wlan_ipa_ws &&
+				!strncmp(ws->name, "wlan_ipa", wslen)) ||
+			(!enable_wlan_pno_wl_ws &&
+				!strncmp(ws->name, "wlan_pno_wl", wslen)) ||
 			(!enable_timerfd_ws &&
 				!strncmp(ws->name, "[timerfd]", wslen))) {
 			if (ws->active) {
